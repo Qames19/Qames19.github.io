91a9ed875458e2c8420c500807a5f1f2885d8641
James Mick <mick.james01@gmail.com>
2021-11-18 17:26:10 -0600

    added styling, javascript, and columns

---
+++ index.js
@@ -0,0 +1,274 @@
+var weather = 
+{
+    "10045": {
+        "zipcode": 10045,
+        "name": "New York",
+        "region": "New York",
+        "country": "USA",
+        "lat": 40.69,
+        "lon": -74.01,
+        "tz_id": "America/New_York",
+        "localtime_epoch": 1637272250,
+        "localtime": "2021-11-18 16:50",
+        "current": {
+            "last_updated_epoch": 1637271900,
+            "last_updated": "2021-11-18 16:45",
+            "temp_c": 20.0,
+            "temp_f": 68.0,
+            "is_day": 0,
+            "condition": {
+                "text": "Partly cloudy",
+                "icon": "./assets/partlyCloudy.jpg",
+                "code": 1003
+            },
+            "wind_mph": 10.5,
+            "wind_kph": 16.9,
+            "wind_degree": 180,
+            "wind_dir": "S",
+            "pressure_mb": 1013.0,
+            "pressure_in": 29.9,
+            "precip_mm": 0.0,
+            "precip_in": 0.0,
+            "humidity": 53,
+            "cloud": 50,
+            "feelslike_c": 20.0,
+            "feelslike_f": 68.0,
+            "vis_km": 16.0,
+            "vis_miles": 9.0,
+            "uv": 5.0,
+            "gust_mph": 14.1,
+            "gust_kph": 22.7
+        }
+    },
+    "33428": {
+        "zipcode": 33428,
+        "name": "Boca Raton",
+        "region": "Florida",
+        "country": "USA",
+        "lat": 26.36,
+        "lon": -80.2,
+        "tz_id": "America/New_York",
+        "localtime_epoch": 1637272617,
+        "localtime": "2021-11-18 16:56",
+        "current": {
+            "last_updated_epoch": 1637271900,
+            "last_updated": "2021-11-18 16:45",
+            "temp_c": 26.1,
+            "temp_f": 79.0,
+            "is_day": 1,
+            "condition": {
+                "text": "Sunny",
+                "icon": "./assets/sunny.jpg",
+                "code": 1003
+            },
+            "wind_mph": 5.6,
+            "wind_kph": 9.0,
+            "wind_degree": 20,
+            "wind_dir": "NNE",
+            "pressure_mb": 1018.0,
+            "pressure_in": 30.07,
+            "precip_mm": 1.0,
+            "precip_in": 0.04,
+            "humidity": 82,
+            "cloud": 75,
+            "feelslike_c": 28.5,
+            "feelslike_f": 83.3,
+            "vis_km": 16.0,
+            "vis_miles": 9.0,
+            "uv": 6.0,
+            "gust_mph": 12.1,
+            "gust_kph": 19.4
+        }
+    },
+    "99571": {
+        "zipcode": 99571,
+        "name": "Cold Bay",
+        "region": "Alaska",
+        "country": "USA",
+        "lat": 55.84,
+        "lon": -161.11,
+        "tz_id": "America/Anchorage",
+        "localtime_epoch": 1637272857,
+        "localtime": "2021-11-18 13:00",
+        "current": {
+            "last_updated_epoch": 1637269200,
+            "last_updated": "2021-11-18 12:00",
+            "temp_c": -7.8,
+            "temp_f": 18.0,
+            "is_day": 1,
+            "condition": {
+                "text": "snowy",
+                "icon": "./assets/snowing.jpg",
+                "code": 1009
+            },
+            "wind_mph": 34.9,
+            "wind_kph": 56.2,
+            "wind_degree": 50,
+            "wind_dir": "NE",
+            "pressure_mb": 1016.0,
+            "pressure_in": 29.99,
+            "precip_mm": 0.0,
+            "precip_in": 0.0,
+            "humidity": 84,
+            "cloud": 100,
+            "feelslike_c": -15.3,
+            "feelslike_f": 4.4,
+            "vis_km": 16.0,
+            "vis_miles": 9.0,
+            "uv": 2.0,
+            "gust_mph": 18.1,
+            "gust_kph": 29.2
+        }
+    },
+    "78254": {
+        "zipcode": 78254,
+        "name": "San Antonio",
+        "region": "Texas",
+        "country": "USA",
+        "lat": 29.54,
+        "lon": -98.72,
+        "tz_id": "America/Chicago",
+        "localtime_epoch": 1637272955,
+        "localtime": "2021-11-18 16:02",
+        "current": {
+            "last_updated_epoch": 1637269200,
+            "last_updated": "2021-11-18 15:00",
+            "temp_c": 15.0,
+            "temp_f": 59.0,
+            "is_day": 1,
+            "condition": {
+                "text": "Overcast",
+                "icon": "./assets/overcast.jpg",
+                "code": 1009
+            },
+            "wind_mph": 12.5,
+            "wind_kph": 20.2,
+            "wind_degree": 30,
+            "wind_dir": "NNE",
+            "pressure_mb": 1027.0,
+            "pressure_in": 30.34,
+            "precip_mm": 0.0,
+            "precip_in": 0.0,
+            "humidity": 36,
+            "cloud": 100,
+            "feelslike_c": 13.1,
+            "feelslike_f": 55.5,
+            "vis_km": 16.0,
+            "vis_miles": 9.0,
+            "uv": 5.0,
+            "gust_mph": 22.1,
+            "gust_kph": 35.6
+        }
+    },
+    "85283": {
+        "zipcode": 85283,
+        "name": "Tempe",
+        "region": "Arizona",
+        "country": "USA",
+        "lat": 33.37,
+        "lon": -111.93,
+        "tz_id": "America/Phoenix",
+        "localtime_epoch": 1637273265,
+        "localtime": "2021-11-18 15:07",
+        "current": {
+            "last_updated_epoch": 1637269200,
+            "last_updated": "2021-11-18 14:00",
+            "temp_c": 27.0,
+            "temp_f": 80.6,
+            "is_day": 1,
+            "condition": {
+                "text": "Thunderstorms",
+                "icon": "./assets/thunderstorm.jpg",
+                "code": 1000
+            },
+            "wind_mph": 0.0,
+            "wind_kph": 0.0,
+            "wind_degree": 0,
+            "wind_dir": "N",
+            "pressure_mb": 1022.0,
+            "pressure_in": 30.17,
+            "precip_mm": 0.0,
+            "precip_in": 0.0,
+            "humidity": 14,
+            "cloud": 0,
+            "feelslike_c": 25.2,
+            "feelslike_f": 77.4,
+            "vis_km": 16.0,
+            "vis_miles": 9.0,
+            "uv": 7.0,
+            "gust_mph": 8.7,
+            "gust_kph": 14.0
+        }
+    },
+    "33587": {
+        "zipcode": 33587,
+        "name": "Sydney",
+        "region": "Florida",
+        "country": "USA",
+        "lat": 27.98,
+        "lon": -82.2,
+        "tz_id": "America/New_York",
+        "localtime_epoch": 1637273414,
+        "localtime": "2021-11-18 17:10",
+        "current": {
+            "last_updated_epoch": 1637269200,
+            "last_updated": "2021-11-18 16:00",
+            "temp_c": 23.0,
+            "temp_f": 73.4,
+            "is_day": 1,
+            "condition": {
+                "text": "Light rain",
+                "icon": "./assets/raining.jpg",
+                "code": 1183
+            },
+            "wind_mph": 0.0,
+            "wind_kph": 0.0,
+            "wind_degree": 0,
+            "wind_dir": "N",
+            "pressure_mb": 1021.0,
+            "pressure_in": 30.15,
+            "precip_mm": 0.0,
+            "precip_in": 0.0,
+            "humidity": 100,
+            "cloud": 100,
+            "feelslike_c": 25.0,
+            "feelslike_f": 77.0,
+            "vis_km": 11.0,
+            "vis_miles": 6.0,
+            "uv": 6.0,
+            "gust_mph": 8.1,
+            "gust_kph": 13.0
+        }
+    }
+};
+
+var imageElement = document.querySelector("img");
+var submitButton = document.getElementById("weather-submit");
+
+var dateInfo = document.getElementById("date-info");
+var timeInfo = document.getElementById("time-info");
+var conditionsInfo = document.getElementById("conditions-info");
+var temperatureInfo = document.getElementById("temperature-info");
+var ambientTempInfo = document.getElementById("ambient-temp-info");
+var windInfo = document.getElementById("wind-info");
+var humidityInfo = document.getElementById("humidity-info");
+var precipitationInfo = document.getElementById("precipitation-info");
+
+submitButton.onclick = fillData;
+
+function fillData(){
+    console.log("button clicked");
+    let value = document.getElementById("weather-zip").value;
+    console.log(weather[value]);
+    imageElement.src = weather[value].current.condition.icon;
+    let dateValue = new Date(weather[value].localtime);
+    dateInfo.innerText = dateValue.getMonth() + "-" + dateValue.getDate() + "-" + dateValue.getFullYear();
+    timeInfo.innerText = dateValue.getHours() + ":" + dateValue.getMinutes();
+    conditionsInfo.innerText = weather[value].current.condition.text;
+    temperatureInfo.innerText = weather[value].current.temp_f + "F";
+    ambientTempInfo.innerText = weather[value].current.feelslike_f + "F";
+    windInfo.innerText = weather[value].current.wind_mph + "mph to the " + weather[value].current.wind_dir;
+    humidityInfo.innerText = weather[value].current.humidity + "%";
+    precipitationInfo.innerText = weather[value].current.precip_in +"\"";
+}
+
